046e85bfc78b27600c5e6a97c619823bc2a68a60
mlyb25 <mlybianto@gmail.com>
2025-02-23 12:11:40 +0800

    update

--- currency-converter/my-react-app/src/App.jsx
+++ currency-converter/my-react-app/src/App.jsx
@@ -1,15 +1,47 @@
-import './App.css'
+import React,{useState} from 'react';
+import Modal from 'react-modal';
+import './App.css';
+
+Modal.setAppElement('#root');
 
 function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+function handleMouseOver () {
+  setModalIsOpen(true);
+}
+
+function closeModal () {
+  setModalIsOpen(false);
+}
 
   return (
     <>
       <h1>Currency Converter</h1>
-      <button>
-        Click me
-      </button>
+      <select name="currency" id="currencySelect" onMouseOver={handleMouseOver}>
+      <option value="usd">USD</option>
+      <option value="jpy">JPY</option>
+      <option value="sgd">SGD</option>
+      <option value="myr">MYR</option>
+      </select>
+
+      <Modal
+  isOpen={modalIsOpen}
+  onRequestClose={closeModal}
+  contentLabel="Alert Modal"
+  className="ReactModal__Content"
+  overlayClassName="ReactModal__Overlay"
+>
+  <div className="modal-header">
+    {`localhost:${window.location.port} says`}
+  </div>
+  <p className="modal-content">Just click!!</p>
+  <div className="modal-footer">
+    <button className="modal-button" onClick={closeModal}>OK</button>
+  </div>
+</Modal>
     </>
-  )
+  );
 }
 
 export default App;
